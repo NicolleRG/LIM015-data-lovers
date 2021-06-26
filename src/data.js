4ff867d1b1082f7e08abab498c25ac62f8623dfe
AlissonCh <rocio.cruz@unmsm.edu.pe>
2021-06-25 22:40:29 -0500

    sort descendente data name, noc y sport

--- src/data.js
+++ src/data.js
@@ -1,45 +1,20 @@
-export const sortDataName = (data) => {
+export const sortData = (data,key,order) => {
         const arrayDataSort = data.sort((a,b)=>{
-          const nameA = a.name.toUpperCase();
-          const nameB = b.name.toUpperCase();
+          const nameA = a[key].toUpperCase();
+          const nameB = b[key].toUpperCase();
           if(nameA<nameB){
-              return -1;
+            //-1
+              return order==='asc'? -1: 1;
           }
           if(nameA>nameB){
-              return 1;
+            //1
+              return order==='asc'? 1: -1;
           }
           return 0;
         });
   return arrayDataSort;
 }
-export const sortDataNoc = (data) => {
-  const arrayDataSort = data.sort((a,b)=>{
-    const propertyA = a.noc.toUpperCase();
-    const propertyB = b.noc.toUpperCase();
-    if(propertyA <propertyB){
-        return -1;
-    }
-    if(propertyA >propertyB){
-        return 1;
-    }
-    return 0;
-  });
-return arrayDataSort;
-}
-export const sortDataSport = (data) => {
-  const arrayDataSort = data.sort((a,b)=>{
-    const propertyA = a.sport.toUpperCase();
-    const propertyB = b.sport.toUpperCase();
-    if(propertyA <propertyB){
-        return -1;
-    }
-    if(propertyA >propertyB){
-        return 1;
-    }
-    return 0;
-  });
-return arrayDataSort;
-}
+
 /*export const anotherExample = () => {
   return 'OMG';
 };*/
--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import {sortDataName , sortDataNoc, sortDataSport} from './data.js';
+import {sortData} from './data.js';
 import data from './data/athletes/athletes.js';
 
 const dataAthletes = data.athletes;// array de objetos de atletas
@@ -41,12 +41,15 @@ function showPreviousData() {
 previousIcon[0].addEventListener('click',showPreviousData);
 
 /*Orden alfabético por noc, nombre, sport */
-const sortIcons = document.querySelectorAll('.sort-icons');
-sortIcons[0].addEventListener('click',function(){ 
+
+const iconUp =document.querySelectorAll('.icon-up');
+const iconDown =document.querySelectorAll('.icon-down');
+
+iconDown[0].addEventListener('click',function(){ 
     rowsNum=0;
     showDataTable(dataAthletes,rowsNum);
-    //console.log(sortDataNoc(dataAthletes));
-    sortDataNoc(dataAthletes).forEach((item,index)=>{
+    //console.log(sortData(dataAthletes,'noc'));
+    sortData(dataAthletes,'noc','desc').forEach((item,index)=>{
         imgColumn1[index].src = `./img-paises/${item.team}.png`;
         nocTextColumn1[index].textContent = item.noc;
         nameColumn2[index].textContent = item.name;
@@ -54,11 +57,11 @@ sortIcons[0].addEventListener('click',function(){
         sportColumn3[index].textContent = item.sport;
     }) 
 })
-sortIcons[1].addEventListener('click',function(){ 
+
+iconDown[1].addEventListener('click',function(){ 
     rowsNum=0;
     showDataTable(dataAthletes,rowsNum);
-    //console.log(sortDataName(dataAthletes));
-    sortDataName(dataAthletes).forEach((item,index)=>{
+    sortData(dataAthletes,'name','desc').forEach((item,index)=>{
         imgColumn1[index].src = `./img-paises/${item.team}.png`;
         nocTextColumn1[index].textContent = item.noc;
         nameColumn2[index].textContent = item.name;
@@ -66,11 +69,11 @@ sortIcons[1].addEventListener('click',function(){
         sportColumn3[index].textContent = item.sport;
     }) 
 })
-sortIcons[2].addEventListener('click',function(){ 
+iconDown[2].addEventListener('click',function(){ 
     rowsNum=0;
     showDataTable(dataAthletes,rowsNum);
-    //console.log(sortDataSport(dataAthletes));
-    sortDataSport(dataAthletes).forEach((item,index)=>{
+    //console.log(sortData(dataAthletes,'noc'));
+    sortData(dataAthletes,'sport','desc').forEach((item,index)=>{
         imgColumn1[index].src = `./img-paises/${item.team}.png`;
         nocTextColumn1[index].textContent = item.noc;
         nameColumn2[index].textContent = item.name;
@@ -78,6 +81,62 @@ sortIcons[2].addEventListener('click',function(){
         sportColumn3[index].textContent = item.sport;
     }) 
 })
+
+iconUp[0].addEventListener('click',function(){ 
+    rowsNum=0;
+    showDataTable(dataAthletes,rowsNum);
+    //console.log(sortData(dataAthletes,'noc'));
+    sortData(dataAthletes,'noc','asc').forEach((item,index)=>{
+        imgColumn1[index].src = `./img-paises/${item.team}.png`;
+        nocTextColumn1[index].textContent = item.noc;
+        nameColumn2[index].textContent = item.name;
+        avatarColumn2[index].textContent = item.gender;
+        sportColumn3[index].textContent = item.sport;
+    }) 
+})
+iconUp[1].addEventListener('click',function(){ 
+    rowsNum=0;
+    showDataTable(dataAthletes,rowsNum);
+    //console.log(sortData(dataAthletes,'name'));
+    sortData(dataAthletes,'name','asc').forEach((item,index)=>{
+        imgColumn1[index].src = `./img-paises/${item.team}.png`;
+        nocTextColumn1[index].textContent = item.noc;
+        nameColumn2[index].textContent = item.name;
+        avatarColumn2[index].textContent = item.gender;
+        sportColumn3[index].textContent = item.sport;
+    }) 
+})
+iconUp[2].addEventListener('click',function(){ 
+    rowsNum=0;
+    showDataTable(dataAthletes,rowsNum);
+    //console.log(sortData(dataAthletes,'sport'));
+    sortData(dataAthletes,'sport','asc').forEach((item,index)=>{
+        imgColumn1[index].src = `./img-paises/${item.team}.png`;
+        nocTextColumn1[index].textContent = item.noc;
+        nameColumn2[index].textContent = item.name;
+        avatarColumn2[index].textContent = item.gender;
+        sportColumn3[index].textContent = item.sport;
+    }) 
+})
+
+function groupBy(key){
+    return function group(array){
+        return array.reduce((acc,obj)=>{
+            const property = obj[key]; 
+            acc[property] = acc[property] || [];
+            acc[property].push(obj);
+            return acc;
+        }, {});
+    };
+}
+const groupByName = groupBy("name");
+groupByName(dataAthletes.slice(0,7));
+console.log(groupByName(dataAthletes.slice(0,7)));
+
+
+
+
+
 /*tenDataAthletes.forEach(item=>{
     tableContent.innerHTML+=`
         <tr>
