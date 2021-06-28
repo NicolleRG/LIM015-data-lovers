10f3e43434f7b5d318baed7b0283f135e601d859
AlissonCh <rocio.cruz@unmsm.edu.pe>
2021-06-28 16:48:27 -0500

    Función sortData mejorada

--- src/data.js
+++ src/data.js
@@ -1,20 +1,6 @@
-export const sortData = (data,key,order) => {
-        const arrayDataSort = data.sort((a,b)=>{
-          const nameA = a[key].toUpperCase();
-          const nameB = b[key].toUpperCase();
-          if(nameA<nameB){
-            //-1
-              return order==='asc'? -1: 1;
-          }
-          if(nameA>nameB){
-            //1
-              return order==='asc'? 1: -1;
-          }
-          return 0;
-        });
-  return arrayDataSort;
-}
-
-/*export const anotherExample = () => {
-  return 'OMG';
-};*/
+export const sortData = (array, order)=> {
+          array.sort((a,b)=>{
+          return a.localeCompare(b);
+       });
+       return order==='desc' ? array.reverse() : array;
+      } 
--- src/main.js
+++ src/main.js
@@ -1,141 +1,91 @@
-import {sortData} from './data.js';
+import { sortData } from './data.js';
 import data from './data/athletes/athletes.js';
-
-const dataAthletes = data.athletes;// array de objetos de atletas
+const dataAthletes = data.athletes;
 const imgColumn1 = document.querySelectorAll('.table-content img');
-const nocTextColumn1 =document.querySelectorAll('.noc-text');
+const nocTextColumn1 = document.querySelectorAll('.noc-text');
 const avatarColumn2 = document.querySelectorAll('.avatar');
 const nameColumn2 = document.querySelectorAll('.name-text');
 const sportColumn3 = document.querySelectorAll('.sport-text');
- 
-const showDataTable = (data,startIndexItem) =>{
-   for(let i = 0; i<20 ; i++){ 
-        const item = data[i+startIndexItem];
-        imgColumn1[i].src = `./img-paises/${item.team}.png`;
-        nocTextColumn1[i].textContent = item.noc;
-        nameColumn2[i].textContent = item.name;
-        avatarColumn2[i].textContent = item.gender;
-        sportColumn3[i].textContent = item.sport;
-    }
-}
-showDataTable(dataAthletes,0);
-
-//const pages = dataAthletes.length/rowsNum; //135 páginas aprox.
-//console.log(pages);
-
+const medalQuantityGold = document.querySelectorAll('.medal-quantity-golden');
+const medalQuantitySilver = document.querySelectorAll('.medal-quantity-silver');
+const medalQuantityBronze = document.querySelectorAll('.medal-quantity-bronze');
 const nextIcon = document.querySelectorAll('.next-icon');
 const previousIcon = document.querySelectorAll('.previous-icon');
-let rowsNum = 0;
-function showNextData() {
-    rowsNum = rowsNum + 20;
-    showDataTable(dataAthletes,rowsNum);
-    //console.log(rowsNum);
-} 
-nextIcon[0].addEventListener('click',showNextData);
-
-function showPreviousData() {
-    rowsNum===0 ? showDataTable(dataAthletes,0): rowsNum=rowsNum-20;
-    //console.log(rowsNum);
-    showDataTable(dataAthletes,rowsNum);
-}
-previousIcon[0].addEventListener('click',showPreviousData);
-
-/*Orden alfabético por noc, nombre, sport */
-
-const iconUp =document.querySelectorAll('.icon-up');
-const iconDown =document.querySelectorAll('.icon-down');
-
-iconDown[0].addEventListener('click',function(){ 
-    rowsNum=0;
-    showDataTable(dataAthletes,rowsNum);
-    //console.log(sortData(dataAthletes,'noc'));
-    sortData(dataAthletes,'noc','desc').forEach((item,index)=>{
-        imgColumn1[index].src = `./img-paises/${item.team}.png`;
-        nocTextColumn1[index].textContent = item.noc;
-        nameColumn2[index].textContent = item.name;
-        avatarColumn2[index].textContent = item.gender;
-        sportColumn3[index].textContent = item.sport;
-    }) 
-})
-
-iconDown[1].addEventListener('click',function(){ 
-    rowsNum=0;
-    showDataTable(dataAthletes,rowsNum);
-    sortData(dataAthletes,'name','desc').forEach((item,index)=>{
-        imgColumn1[index].src = `./img-paises/${item.team}.png`;
-        nocTextColumn1[index].textContent = item.noc;
-        nameColumn2[index].textContent = item.name;
-        avatarColumn2[index].textContent = item.gender;
-        sportColumn3[index].textContent = item.sport;
-    }) 
-})
-iconDown[2].addEventListener('click',function(){ 
-    rowsNum=0;
-    showDataTable(dataAthletes,rowsNum);
-    //console.log(sortData(dataAthletes,'noc'));
-    sortData(dataAthletes,'sport','desc').forEach((item,index)=>{
-        imgColumn1[index].src = `./img-paises/${item.team}.png`;
-        nocTextColumn1[index].textContent = item.noc;
-        nameColumn2[index].textContent = item.name;
-        avatarColumn2[index].textContent = item.gender;
-        sportColumn3[index].textContent = item.sport;
-    }) 
-})
-
-iconUp[0].addEventListener('click',function(){ 
-    rowsNum=0;
-    showDataTable(dataAthletes,rowsNum);
-    //console.log(sortData(dataAthletes,'noc'));
-    sortData(dataAthletes,'noc','asc').forEach((item,index)=>{
-        imgColumn1[index].src = `./img-paises/${item.team}.png`;
-        nocTextColumn1[index].textContent = item.noc;
-        nameColumn2[index].textContent = item.name;
-        avatarColumn2[index].textContent = item.gender;
-        sportColumn3[index].textContent = item.sport;
-    }) 
-})
-iconUp[1].addEventListener('click',function(){ 
-    rowsNum=0;
-    showDataTable(dataAthletes,rowsNum);
-    //console.log(sortData(dataAthletes,'name'));
-    sortData(dataAthletes,'name','asc').forEach((item,index)=>{
-        imgColumn1[index].src = `./img-paises/${item.team}.png`;
-        nocTextColumn1[index].textContent = item.noc;
-        nameColumn2[index].textContent = item.name;
-        avatarColumn2[index].textContent = item.gender;
-        sportColumn3[index].textContent = item.sport;
-    }) 
-})
-iconUp[2].addEventListener('click',function(){ 
-    rowsNum=0;
-    showDataTable(dataAthletes,rowsNum);
-    //console.log(sortData(dataAthletes,'sport'));
-    sortData(dataAthletes,'sport','asc').forEach((item,index)=>{
-        imgColumn1[index].src = `./img-paises/${item.team}.png`;
-        nocTextColumn1[index].textContent = item.noc;
-        nameColumn2[index].textContent = item.name;
-        avatarColumn2[index].textContent = item.gender;
-        sportColumn3[index].textContent = item.sport;
-    }) 
-})
+const iconUp = document.querySelectorAll('.icon-up');
+const iconDown = document.querySelectorAll('.icon-down');
 
 function groupBy(key){
-    return function group(array){
-        return array.reduce((acc,obj)=>{
-            const property = obj[key]; 
+    return function group(data){
+        return data.reduce((acc,item)=>{
+            const property = item[key]; 
             acc[property] = acc[property] || [];
-            acc[property].push(obj);
+            acc[property].push(item);
             return acc;
         }, {});
     };
 }
-const groupByName = groupBy("name");
-groupByName(dataAthletes.slice(0,7));
-console.log(groupByName(dataAthletes.slice(0,7)));
+const groupByName = groupBy('name');
+const newData = groupByName(dataAthletes);
+const arrayOfNames = Object.keys(newData); // [names]
+
+function showMedalQuantity (array , typeMedal) {
+    const medalArray = array.map(element=>element.medal);
+    const typeMedalArray = medalArray.filter(element=>element===typeMedal);
+    return typeMedalArray.length>0? typeMedalArray.length : '-';
+}
+function showDataTable(array , startIndexProperty , data){
+    //console.log(array);
+    let i = 0;
+    for (let key in data){
+        key = array[i+startIndexProperty];
+        //console.log(key);
+        const arrayInProperty = data[key]; //[{},{},{}]
+            imgColumn1[i].src = `./img-paises/${arrayInProperty[0].team}.png`;
+            nocTextColumn1[i].textContent = arrayInProperty[0].noc;
+            nameColumn2[i].textContent = arrayInProperty[0].name;
+            avatarColumn2[i].textContent = arrayInProperty[0].gender;
+            sportColumn3[i].textContent = arrayInProperty[0].sport;
+            medalQuantityGold[i].textContent = showMedalQuantity(arrayInProperty, 'Gold');
+            medalQuantitySilver[i].textContent = showMedalQuantity(arrayInProperty, 'Silver');
+            medalQuantityBronze[i].textContent = showMedalQuantity(arrayInProperty, 'Bronze');
+        i<19? i++ : false;
+    }
+}
+showDataTable(arrayOfNames , 0 , newData);
+
+let index = 0;
+nextIcon[0].addEventListener('click', () => {
+    index = index + 20;
+    showDataTable(arrayOfNames , index , newData);
+    //console.log(index);  
+});
+previousIcon[0].addEventListener('click', () => {
+    index===0 ? showDataTable(0): index = index-20;
+    //console.log(index);
+    showDataTable(arrayOfNames , index , newData);
+});
+
+/*Orden alfabético por nombre */
+index=0;
+const sortName = order =>{
+    index = 0;
+    showDataTable(sortData(arrayOfNames,order),index,newData)
+}
+iconDown[1].addEventListener('click', () => sortName('asc'))
+iconUp[1].addEventListener('click', () => sortName('desc'));
 
 
-
-
+// const showDataInTable = (data , startIndexItem) =>{
+//    for(let i = 0; i<20 ; i++){ 
+//         const item = data[i+startIndexItem];
+//         imgColumn1[i].src = `./img-paises/${item.team}.png`;
+//         nocTextColumn1[i].textContent = item['noc'];
+//         nameColumn2[i].textContent = item.name;
+//         avatarColumn2[i].textContent = item.gender;
+//         sportColumn3[i].textContent = item.sport;
+//     }
+// }
+// showDataInTable(dataAthletes , 0);
 
 /*tenDataAthletes.forEach(item=>{
     tableContent.innerHTML+=`
